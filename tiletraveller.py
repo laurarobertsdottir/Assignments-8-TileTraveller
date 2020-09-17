50be912c9d3821f0954093f784040c544cda6627
hildurottosdottir <hildurho20@ru.is>
2020-09-17 12:17:48 +0000

    laga

--- tiletraveller.py
+++ tiletraveller.py
@@ -9,97 +9,119 @@ start = (1,1)
 print("You can travel: ", átt)
 
 while start != (3,1):
-    direction = input("Direction: ")
     if start == (1,1):    
+        direction = input("Direction: ")
         if direction == "N" or direction == "n":
             start = (1,2)
             átt = "(N)orth or (E)ast or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else: 
             print("Not a valid direction!")
             start = start
+            direction = input("Direction: ")
     if start == (1,2):
         if direction == "N" or direction == "n":
             start = (1,3)
             átt = "(E)ast or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         elif direction == "E" or direction == "e":
             start = (2,2)
             átt = "(W)est or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         elif direction == "S" or direction == "s":
             start = (1,1)
             átt = "(N)orth."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else:
             print("Not a valid direction!")
             start = start
+            direction = input("Direction: ")
     if start == (1,3):
         if direction == "E" or direction == "e":
             start = (2,3)
             átt = "(E)ast or (W)est"
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         elif direction == "S" or direction == "s":
             start = (1,2)
             átt = "(N)orth or (E)ast or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else:
             print("Not a valid direction!")
             start = start 
+            direction = input("Direction: ")
     if start == (2,3):
         if direction == "E" or direction == "e":
             start = (3,3)
             átt = "(S)outh or (W)est."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         elif direction == "W" or direction =="w":
             start = (1,3)  
             átt = "(E)ast or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else:
             print("Not a valid direction!")
             start = start 
+            direction = input("Direction: ")
     if start == (2,2):
         if direction == "S" or direction == "s":
             start = (2,1)
             átt = "(N)orth."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         elif direction == "W" or direction == "w":
             start = (1,2)
             átt = "(N)orth or (E)ast or (S)outh."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else:
             print("Not a valid direction!")
             start = start 
+            direction = input("Direction: ")
     if start == (2,1):
        if direction == "n" or direction =="N":
            start = (2,2)
            átt = "(S)outh or (W)est."    
-           print("You can travel: ",átt)   
+           print("You can travel: ",átt)  
+           direction = input("Direction: ") 
        else:
            print("Not a valid direction!")
            start = start
+           direction = input("Direction: ")
     if start == (3,3):
         if direction == "W" or direction == "w":
             start = (2,3)
             átt = "(E)ast or (W)est."    
             print("You can travel: ",átt)
+            direction = input("Direction: ")
         elif direction == "S" or direction == "s":
             start = (3,2)
             átt = "(N)orth or (S)outh."
             print("You can travel: ",átt)
+            direction = input("Direction: ")
         else: 
             print("Not a valid direction!")
             start = start
+            direction = input("Direction: ")
     if start == (3,2):
         if direction == "S" or direction =="s":
-            start = (3,2)
+            start = (3,1)
         elif direction == "N" or direction == "n":
             start = (3,3)
             átt = "(S)outh or (W)est."
             print("You can travel: ", átt)
+            direction = input("Direction: ")
         else:
             print("Not a valid direction!")
             start = start
+            direction = input("Direction: ")
     if start == (3,1):
         print("Victory!")
 
