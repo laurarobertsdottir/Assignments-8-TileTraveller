902cda238df606af2e52f99ce324bce82d65a550
hildurottosdottir <hildurho20@ru.is>
2020-09-17 10:41:51 +0000

    save

--- tiletraveller.py
+++ tiletraveller.py
@@ -5,7 +5,7 @@ print("You can travel:", átt)
 while start != (3,1):
     if star == (1,1):
         direction = input("Direction: ")
-        if direction = "n":
+        if direction = "N":
             start = (1,2)
             átt = "(N)orth or (E)ast or (S)outh."
             print("You can travel:", átt)
@@ -13,4 +13,14 @@ while start != (3,1):
         else: 
             print("Not a valid direction!")
             start = start
+    if start == (1,2):
+        if direction == "N":
+            start = (1,3)
+            átt = "(E)ast or (S)outh."
+            direction = input("Direction:")
+        elif: direction == "E":
+            start = (2,2)
+            átt = "(W)est or (S)outh."
+            direction = input("Direction:")
+        
     
